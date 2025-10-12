d56e4c1c8affac7b30f9c660d8076ff7bea4b663
abhi06032005 <abhijeetnayak478@gmail.com>
2025-10-12 16:21:35 +0530

    turbo json

--- apps/fe/config.ts
+++ apps/fe/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = process.env.BACKEND_URL
-export const WS_URL =process.env.WS_URL
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL
+export const WS_URL =process.env.NEXT_PUBLIC_WS_URL
