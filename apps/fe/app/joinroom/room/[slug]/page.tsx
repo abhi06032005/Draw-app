1ceafac84ceba0ff602fad6ff02ff8b793a45023
abhi06032005 <abhijeetnayak478@gmail.com>
2025-10-12 19:14:06 +0530

    props type to any

--- apps/fe/app/joinroom/room/[slug]/page.tsx
+++ apps/fe/app/joinroom/room/[slug]/page.tsx
@@ -1,49 +1,34 @@
-
 import axios from "axios";
 import { BACKEND_URL } from "../../../../config";
-import { redirect} from "next/navigation";
-
+import { redirect } from "next/navigation";
 import { CanvasRoom } from "../../CanvasRoom";
 
-
-
 async function getRoomId(slug: string) {
-
-    
-   try {
-        const response = await axios.get(`${BACKEND_URL}/room/${slug}`)
-        if (response.status === 200 ){
-           return response.data.roomId;
-        }
+  try {
+    const response = await axios.get(`${BACKEND_URL}/room/${slug}`);
+    if (response.status === 200) {
+      return response.data.roomId;
     }
-    catch(e){
-        return null
-    }
-
+  } catch (e) {
+    return null;
+  }
 }
 
-type Props = {
-  params: {
-    slug: string;
-  };
-};
-export default async function Page({params}:Props) {
-    const slug = params.slug;
-    const roomId = await getRoomId(slug);
+export default async function Page ({
+  params,
+}: {
+  params: any;  // keep any to avoid type conflict
+}) {
+  const slug = params.slug;  // no await here
+  const roomId = await getRoomId(slug);
 
-    if(roomId == null){
-        redirect('/create')
-    }
-
-    else{
-        return <div>
-            <CanvasRoom roomId={roomId}></CanvasRoom>
-        
-        </div>
-    }
-   
-    
-
-    
-
+  if (roomId == null) {
+    redirect("/create");
+  } else {
+    return (
+      <div>
+        <CanvasRoom roomId={roomId}></CanvasRoom>
+      </div>
+    );
+  }
 }
