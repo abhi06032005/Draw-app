d3a10155088950db2b2ffb208e40f69cd114b433
abhi06032005 <abhijeetnayak478@gmail.com>
2025-10-12 19:01:33 +0530

    params fetch

--- apps/fe/app/joinroom/room/[slug]/page.tsx
+++ apps/fe/app/joinroom/room/[slug]/page.tsx
@@ -29,7 +29,7 @@ export default async function Page({
         slug: string
     }
 }) {
-    const slug = (await params).slug;
+    const slug = params.slug;
     const roomId = await getRoomId(slug);
 
     if(roomId == null){
